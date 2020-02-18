7a6899e931b1ec01f0bfc381a35083f35d2f0655
Xiong Ben <xiongben1993@gmail.com>
2020-02-18 18:06:00 +0800

    [add]

--- router.js
+++ router.js
@@ -2,11 +2,39 @@ import React from 'react';
 // import { NavigationContainer } from '@react-navigation/native';
 // import { createStackNavigator } from '@react-navigation/stack';
 import { createAppContainer } from 'react-navigation';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
 import { createStackNavigator } from 'react-navigation-stack';
 
 import Home from './pages/home';
 import List from './pages/list';
 import Mypage from './pages/mypage';
+
+import { Text, View } from 'react-native';
+
+class HomeScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Home!</Text>
+      </View>
+    );
+  }
+}
+
+class SettingsScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Settings!</Text>
+      </View>
+    );
+  }
+}
+
+const TabNavigator = createBottomTabNavigator({
+  Home: { screen: HomeScreen },
+  Settings: { screen: SettingsScreen },
+});
  
 // const  RouteApp = StackNavigator({
 //     Home: { screen: Home },
@@ -35,5 +63,5 @@ const RouteApp = createStackNavigator({
     },
   });
   
-export default createAppContainer(RouteApp);
+export default createAppContainer(TabNavigator);
   
